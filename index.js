e4b439f24b054bd224928fee5f197a3957333deb
maxjamin <maxjamin@gmail.com>
2019-02-26 19:49:24 -0700

    adding main to index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/response', (req, res) => res.render('pages/response'))
   .get('/getMail', function(req, res) {
-  	console.log(request.query.postalType);
+  	var x =request.query.postalType;
 
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
