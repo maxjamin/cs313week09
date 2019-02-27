8db91a6d1f4150dd429fd2d1121c63698e0b3699
maxjamin <maxjamin@gmail.com>
2019-02-26 20:19:29 -0700

    adding main to index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-node.get('/getMail', calculateRate)
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,19 +9,13 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/response', (req, res) => res.render('pages/response'))
- 
+
+  .get('/getMail', (req, res) => {
+  		
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 
 
 
-
-
-
-
-
-function calculateRate(request, response) {
-
-		
-	}
